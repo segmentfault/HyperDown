76d4275a5f14ed74faf9513dd547cfac280d35f1
DXkite <DXkite@163.com>
2016-09-05 12:43:45 +0800

    当长度为0时会导致索引错误

--- Parser.php
+++ Parser.php
@@ -998,8 +998,8 @@ class Parser
 
             if ($line[0] == '|') {
                 $line = substr($line, 1);
-
-                if ($line[strlen($line) - 1] == '|') {
+				$len=strlen($line);
+                if ($len && $line[$len - 1] == '|') {
                     $line = substr($line, 0, -1);
                 }
             }
